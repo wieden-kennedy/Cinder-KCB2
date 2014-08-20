270915fbc7af48b315f25e20489510c736e3ea0f
bantherewind <bantherewind@gmail.com>
2014-08-20 16:35:15 -0700

    Added Tinderbox template

---
+++ templates/Basic KCB2/src/_TBOX_PREFIX_App.cpp
@@ -0,0 +1,35 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/Texture.h"
+
+#include "Kinect2.h"
+
+class _TBOX_PREFIX_App : public ci::app::AppBasic 
+{
+public:
+	void draw();
+	void setup();
+private:
+	ci::Channel16u mChannel;
+	Kinect2::DeviceRef mDevice;
+};
+
+void _TBOX_PREFIX_App::draw()
+{
+	if ( mChannel ) {
+		ci::gl::TextureRef tex = ci::gl::Texture::create( Kinect2::channel16To8( mChannel ) );
+		ci::gl::draw( tex, tex->getBounds(), getWindowBounds() );
+	}
+}
+
+void _TBOX_PREFIX_App::setup()
+{	
+	mDevice = Kinect2::Device::create();
+	mDevice->start();
+	mDevice->connectDepthEventHandler( [ & ]( const Kinect2::DepthFrame& frame )
+	{
+		mChannelDepth = frame.getChannel();
+	} );
+}
+
+CINDER_APP_BASIC( _TBOX_PREFIX_App, ci::app::RendererGl )
+	
